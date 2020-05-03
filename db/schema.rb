2c74f6ad68a748efe2b06bb2a1fa5fb142aab2ea
Valkryst <valkryst@valkryst.com>
2020-05-03 14:13:05 -0300

    * Add `:description` to the `Post` model * Add search functionality to `posts/index.html.erb` view * Add the `Category` model * Fix issue where the footer element wasn't being placed at the bottom of the page * Modify `:body_html` to change the type from `string` to `text` * Modify the `Post` model to allow `:index_html` to be blank * Populate `seeds.rb` * Redesign the `posts/index.html.erb` view * Remove the `tzinfo-data` gem as it's not required

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_02_172144) do
+ActiveRecord::Schema.define(version: 2020_05_03_223921) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
-    t.string "body_html"
+    t.text "body_html"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
     t.string "index_html"
+    t.integer "category_id"
+    t.text "description"
+    t.index ["category_id"], name: "index_posts_on_category_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
@@ -64,6 +73,7 @@ ActiveRecord::Schema.define(version: 2020_05_02_172144) do
     t.string "role", default: "reader"
   end
 
+  add_foreign_key "posts", "categories"
   add_foreign_key "posts", "users"
   add_foreign_key "taggings", "tags"
 end
