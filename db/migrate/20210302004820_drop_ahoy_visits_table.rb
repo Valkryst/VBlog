b097d555805bbe433f380302ccb97854ae59591a
Valkryst <github@valkryst.com>
2023-05-06 13:10:51 -0300

    Remove `ahoy` related migrations

--- db/migrate/20210302004820_drop_ahoy_visits_table.rb
+++
@@ -1,5 +0,0 @@
-class DropAhoyVisitsTable < ActiveRecord::Migration[6.0]
-  def change
-    drop_table :ahoy_events
-  end
-end
--- db/migrate/20210302004833_drop_ahoy_events_table.rb
+++
@@ -1,5 +0,0 @@
-class DropAhoyEventsTable < ActiveRecord::Migration[6.0]
-  def change
-    drop_table :ahoy_visits
-  end
-end
