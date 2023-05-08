336e2545f943534934ef8cc6b4624834491927d8
Valkryst <github@valkryst.com>
2023-05-08 17:55:24 -0300

    Remove unnecessary `config.hosts` entry

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,5 +110,5 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  Rails.application.config.hosts = ['valkryst.com', 'www.valkryst.com']
+  config.hosts << 'www.valkryst.com'
 end
