5a64fcd693b95de7174575f80d14306456ac60dd
Valkryst <github@valkryst.com>
2025-06-07 23:07:48 -0300

    Update `PostsRSSLinkElement` to open the `posts.rss` page in a new tab

--- element/posts/rss_link/.js
+++ element/posts/rss_link/.js
@@ -1,7 +1,7 @@
 const template = document.createElement('template');
 template.innerHTML = `
 <link rel="stylesheet" href="${import.meta.resolve('/element/posts/rss_link/.css')}">
-<a href="/VBlog/posts.rss">
+<a href="/VBlog/posts.rss" target="_blank">
     <img src="/VBlog/resource/icon/rss.png" alt="RSS Icon">
     Subscribe to RSS Feed
 </a>
